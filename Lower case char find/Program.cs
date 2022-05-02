5151ae8b03e0fd2d5ab402983e1da677bb139a75
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-05-03 02:04:01 +0530

    Add project files.

---
+++ Lower case char find/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lower_case_char_find
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the value :");
+            char data = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("This is value {0} ", data);
+
+            int f = data-32;
+
+            char result = (char)f;
+
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
